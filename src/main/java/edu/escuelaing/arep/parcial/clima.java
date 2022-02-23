fd53495c45d88136e02e6a116ae06248bdcc4679
Cristhian Torres <cristhian.torres@mail.escuelaing.edu.co>
2022-02-23 14:14:01 -0500

    httpserver

---
+++ src/main/java/edu/escuelaing/arep/parcial/clima.java
@@ -0,0 +1,9 @@
+package edu.escuelaing.arep.parcial;
+
+
+public class clima {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/edu/escuelaing/arep/parcial/server/HttpServer.java
@@ -0,0 +1,72 @@
+package edu.escuelaing.arep.parcial.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+
+public class HttpServer {
+    private int port;
+    private static URL url;
+
+
+    static {
+        try {
+            url = new URL("https://openweathermap.org/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startServer(int httpPort) throws IOException {
+        port = httpPort;
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(getPort());
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+
+        boolean running = true;
+        while (running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir en puerto "+getPort()+" ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+            outputLine = url.toString();
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+}
